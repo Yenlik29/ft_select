e8b37d90d553942f4ab66440c566fee3615112b2
Yenlik BOKINA <ybokina@e1r4p1.unit.ua>
2018-09-05 11:05:25 +0300

    h

--- ft_print1.c
+++ ft_print1.c
@@ -12,32 +12,40 @@
 
 #include "ft_select.h"
 
+void			print_dif(int dif)
+{
+	while (dif)
+	{
+		ft_putstr(" ");
+		dif--;
+	}
+}
+
 void			print_norm(t_arg *args, int row_q, int col_q)
 {
 	int		c;
-	t_arg	*tmp;
 	int		dif;
+	t_arg	*tmp;
 
 	c = col_q;
 	tmp = args;
-	while (tmp != NULL)
+	while (tmp)
 	{
 		while (row_q--)
 		{
-			col_q = c;
 			while (col_q--)
 			{
-				ft_putstr(tmp->name);
-				if (max_strlen(args) > (int)ft_strlen(tmp->name))
-					dif = max_strlen(args) - (int)ft_strlen(tmp->name);
-				while (dif)
+				if (tmp)
 				{
-					ft_putstr(" ");
-					dif--;
+					ft_putstr(tmp->name);
+					if (max_strlen(args) >= (int)ft_strlen(tmp->name))
+						dif = max_strlen(args) - (int)ft_strlen(tmp->name);
+					print_dif(dif);
+					tmp = tmp->next;
+					ft_putstr("  ");
 				}
-				tmp = tmp->next;
-				ft_putstr("  ");
 			}
+			col_q = c;
 			ft_putstr("\n");
 		}
 	}
--- ft_select.h
+++ ft_select.h
@@ -58,6 +58,7 @@ void			print_list(t_arg *lst);
 t_arg			*argv_init(char **argv);
 
 void			print_norm(t_arg *args, int row_q, int col_q);
+void			print_dif(int dif);
 
 void			print_argv(char **argv);
 int 			ft_count(char **argv);
